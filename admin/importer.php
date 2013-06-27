9e8cc50d71212061663d70e8f060ecdfdbaad665
ekaputra07 <ekaputra07@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-27 14:56:22 +0000

    Edit importer howto
    git-svn-id: https://plugins.svn.wordpress.org/wpe-indoshipping/trunk@732886 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/importer.php
+++ admin/importer.php
@@ -7,12 +7,11 @@
 <div class="slide-block">
 <div class="module-block">
 <!-- start module content Admin-->
-<p>Dengan DB importer ini anda dapat mengimport seluruh data shipping (Provinsi & Kota) dari file <strong>daerah.db.php</strong> dan akan secara otomatis diimport ke dalam database WPE Indoshipping Pro ini.</p>
-<p>Silahkan download <a href="https://dl.dropbox.com/u/40229563/daerah.zip"/><strong>disini</strong></a> (extract terlebih dahulu).</p>
-
-<p>
-<strong>PERHATIAN :</strong><br />Setiap operasi Import tidak akan menghapus semua data shipping yang sudah ada di Database terlebih dahulu, namun hanya akan menambahkan data Provinsi yang belum ada di database. Untuk itu untuk melakukkan Import secara Full semua data, maka data shipping yang sekarang perlu dikosongkan terlebih dahulu melalui menu <strong>Indoshipping -> Manage -> Hapus semua data</strong>. Pastikan juga file daerah.db.php memiliki isi yang valid.</p>
-<p>Upload file <strong>daerah.db.php</strong> melalui uploader di bawah ini, dan click Import.</p>
+<p>Dengan DB importer ini anda dapat mengimport seluruh data shipping (Provinsi & Kota) dari file <strong>CSV</strong> dan akan secara otomatis diimport ke dalam database.</p><p>
+<strong>PERHATIAN :</strong><br />Setiap operasi Import tidak akan menghapus semua data shipping yang sudah ada di Database terlebih dahulu, namun hanya akan menambahkan data Provinsi yang belum ada di database.</p>
+<p>Untuk itu untuk melakukkan Import secara Full semua data, maka data shipping yang sekarang perlu dikosongkan terlebih dahulu melalui menu <strong>Indoshipping -> Manage -> Hapus semua data</strong>.</p>
+<p>Pastikan format daftar ongkos kirim di file CSV sesuai format yang telah ditentukan. <a href="<?=$app_base_url ?>sample.zip">Download contoh disini</a>.</p>
+<p>Upload file <strong>CSV</strong> melalui uploader di bawah ini, dan click Import.</p>
 
 <?php
 global $upload_path,$upload_action;
@@ -35,43 +34,46 @@ if(!is_writable($upload_path)){
 			name: 'uploadfile',
 			data:({upload_path:'<?=$upload_path?>'}),
 			onSubmit: function(file, ext){
-				 if (! (ext && /^(php)$/.test(ext))){
+				 if (! (ext && /^(csv)$/.test(ext))){
                     // extension is not allowed
-					status.text('Only PHP file are allowed');
+					status.text('Error: Hanya file CSV yang diijinkan!');
 					return false;
 				}
-				status.text('Uploading, please wait...');
+				status.text('Mengupload, silahkan tunggu...');
 			},
-			onComplete: function(file, response){
+			onComplete: function(file, resp){
+                $('#import_file').val(file);
+                status.text('');
+                /*
 				//On completion clear the status
 				status.text('');
 				//Add uploaded file to list
 				if(response.length < 25 && response != 'error'){
 				    $('#import_file').val(file);
-					status.text('Upload success!');
+					status.text('Upload berhasil!');
 				} else{
 					status.text('Upload error!');
-				}
+				}*/
 			}
 		});
 
     $('#btn-import').click(function(){
         var dbfile = $('#import_file').val();
         if(dbfile != ''){
-        $('.import-status').text('Start importing...');
+        $('.import-status').text('mengimport...');
         $.ajax({
             type : 'post',
-            url : indoshipping_pro_vars.ajaxurl,
+            url : indoshipping_vars.ajaxurl,
             data :({action:'DBIMPORT','dbfile':dbfile}),
             success : function(respond){
-            $('.import-status').text(respond);
+            $('.import-status').html(respond);
             },
             error : function(){
-                $('.import-status').text('Import failed!.');
+                $('.import-status').text('Import gagal!.');
             }
         });
         }else{
-        alert('Upload file daerah.db.php terlebih dahulu!');
+        alert('Upload file CSV terlebih dahulu!');
         }
     });
 
@@ -79,7 +81,8 @@ if(!is_writable($upload_path)){
 </script>
 <!-- END JS FOR AJAX UPLOADER-->
 <p><input type="text" name="import_file" id="import_file" value="" readonly="readonly"> <input type="button" value="Upload" id="btn-upload" class="button-secondary"/> <span class="upload-status"></span></p>
-<p><input type="button" value="Import sekarang" id="btn-import" class="button-primary"/> <span class="import-status"></span></p>
+<p><input type="button" value="Import sekarang" id="btn-import" class="button-primary"/><br/><br/>
+<span class="import-status"></span></p>
 <!-- End module content Admin -->
 </div>
 </div></div></div>
